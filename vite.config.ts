6ab9750d91fe3a9a5a96c1a3500c7aa7b12498c9
cpl121 <cpldev121@gmail.com>
2023-04-11 19:05:49 +0200

    fix: try to deploy in fleek

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), mdPlugin({ mode: [Mode.HTML] })],
+	base: '',
 	css: {
 		preprocessorOptions: {
 			scss: {
